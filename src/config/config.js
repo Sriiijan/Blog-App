cfb58fd1808e1a0e493617a7dd7be432b7eff02a
Srijan Sasmal <srijansasmal88@gmail.com>
2025-08-10 01:47:45 +0530

    Initial commit after reconnect

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const config= {
+    apperiteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    apperiteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    apperiteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    apperiteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_IDL),
+    apperiteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default config
